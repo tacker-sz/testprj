c0b06b8060fab831d5ba8c9228407c1c81f06041
Takahiro Suzuki <takahiro.jr.suzuki@nts.ricoh.co.jp>
2017-09-11 16:38:49 +0900

    hedder

--- seqtime.h
+++ seqtime.h
@@ -18,7 +18,7 @@ enum
 #define	TRAN_IL_TIME		1
 #define	LOW_DRIVE_TIME		10
 #define	TRAN_LH_TIME		1
-#define	HIGH_DRIVE			1
+#define	HIGH_DRIVE_TIME		1
 #define	TRAN_HL_TIME		1
 
 #define	LOW_POWER_LEVEL		40	//MIN= 0
